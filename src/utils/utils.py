74470b65a4387284b4b05fdb0c773bd54da8986d
nibilis <nhenriquesdealmeida@gmail.com>
2025-09-28 23:59:05 -0300

    Add doc file and fix to show_file_content function

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -8,8 +8,15 @@
 # File history:
 # - 2025-09-27: Created the file and added the initial code.
 
+import os
+
 def show_file_content(filename):
     try:
+        base_path = os.path.dirname(__file__)
+        full_path = os.path.join(base_path, "..", "resources", filename)
+        full_path = os.path.abspath(full_path)
+
+        print(f"Reading file from: {full_path}")
         with open(filename, 'r') as file:
             content = file.read()
             print("File Content:")
